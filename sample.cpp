93bc9778e38b16cab2b95cc09ff95be0090d2054
Lam Jouq Hang Hendersen <hendersenlam@gmail.com>
2017-03-08 02:36:02 +0800

    Comment out main to avoid clash with bastion.cpp

--- sample.cpp
+++ sample.cpp
@@ -70,18 +70,18 @@ void SampleModel::draw()
 	glPopMatrix();
 }
 
-int main()
-{
-	// Initialize the controls
-	// Constructor is ModelerControl(name, minimumvalue, maximumvalue, 
-	// stepsize, defaultvalue)
-    ModelerControl controls[NUMCONTROLS];
-    controls[XPOS] = ModelerControl("X Position", -5, 5, 0.1f, 0);
-    controls[YPOS] = ModelerControl("Y Position", 0, 5, 0.1f, 0);
-    controls[ZPOS] = ModelerControl("Z Position", -5, 5, 0.1f, 0);
-    controls[HEIGHT] = ModelerControl("Height", 1, 2.5, 0.1f, 1);
-	controls[ROTATE] = ModelerControl("Rotate", -135, 135, 1, 0);
-
-    ModelerApplication::Instance()->Init(&createSampleModel, controls, NUMCONTROLS);
-    return ModelerApplication::Instance()->Run();
-}
+//int main()
+//{
+//	// Initialize the controls
+//	// Constructor is ModelerControl(name, minimumvalue, maximumvalue, 
+//	// stepsize, defaultvalue)
+//    ModelerControl controls[NUMCONTROLS];
+//    controls[XPOS] = ModelerControl("X Position", -5, 5, 0.1f, 0);
+//    controls[YPOS] = ModelerControl("Y Position", 0, 5, 0.1f, 0);
+//    controls[ZPOS] = ModelerControl("Z Position", -5, 5, 0.1f, 0);
+//    controls[HEIGHT] = ModelerControl("Height", 1, 2.5, 0.1f, 1);
+//	controls[ROTATE] = ModelerControl("Rotate", -135, 135, 1, 0);
+//
+//    ModelerApplication::Instance()->Init(&createSampleModel, controls, NUMCONTROLS);
+//    return ModelerApplication::Instance()->Run();
+//}
