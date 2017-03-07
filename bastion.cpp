326b108415fb47710710512dda9fbeb4293ddd9d
Lam Jouq Hang Hendersen <hendersenlam@gmail.com>
2017-03-07 17:59:04 +0800

    Added main file bastion.cpp

---
+++ bastion.cpp
@@ -0,0 +1,60 @@
+#include "modelerview.h"
+#include "modelerapp.h"
+#include "modelerdraw.h"
+#include <FL/gl.h>
+
+#include "modelerglobals.h"
+
+// To make a SampleModel, we inherit off of ModelerView
+class BastionModel : public ModelerView
+{
+public:
+	BastionModel(int x, int y, int w, int h, char *label)
+		: ModelerView(x, y, w, h, label) { }
+
+	virtual void draw();
+};
+
+// We need to make a creator function, mostly because of
+// nasty API stuff that we'd rather stay away from.
+ModelerView* createSampleModel(int x, int y, int w, int h, char *label)
+{
+	return new BastionModel(x, y, w, h, label);
+}
+
+// We are going to override (is that the right word?) the draw()
+// method of ModelerView to draw out SampleModel
+void BastionModel::draw()
+{
+	// This call takes care of a lot of the nasty projection 
+	// matrix stuff.  Unless you want to fudge directly with the 
+	// projection matrix, don't bother with this ...
+	ModelerView::draw();
+
+	// draw the floor
+	setAmbientColor(.1f, .1f, .1f);
+	setDiffuseColor(COLOR_RED);
+	glPushMatrix();
+	glTranslated(-5, 0, -5);
+	drawBox(10, 0.01f, 10);
+	glPopMatrix();
+
+	// draw the bastion model
+
+}
+
+int main()
+{
+	// Initialize the controls
+	// Constructor is ModelerControl(name, minimumvalue, maximumvalue, 
+	// stepsize, defaultvalue)
+	ModelerControl controls[NUMCONTROLS];
+	controls[XPOS] = ModelerControl("X Position", -5, 5, 0.1f, 0);
+	controls[YPOS] = ModelerControl("Y Position", 0, 5, 0.1f, 0);
+	controls[ZPOS] = ModelerControl("Z Position", -5, 5, 0.1f, 0);
+	controls[HEIGHT] = ModelerControl("Height", 1, 2.5, 0.1f, 1);
+	controls[ROTATE] = ModelerControl("Rotate", -135, 135, 1, 0);
+
+	ModelerApplication::Instance()->Init(&createSampleModel, controls, NUMCONTROLS);
+	return ModelerApplication::Instance()->Run();
+}
